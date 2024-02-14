ae64c689bef91f46c5b970fbfafcaaa10755ef2e
Inamul Hassan <hassan.inamul@outlook.com>
2024-02-14 12:31:40 +0530

    Complete GET

--- book/book.py
+++ book/book.py
@@ -14,3 +14,34 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    book = next((book for book in BOOKS
+                 if book['title'].casefold() == book_title.casefold()), None)
+    if book is not None:
+        return book
+    else:
+        return {'error': 'Book not found'}
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books = [book for book in BOOKS
+             if book['category'].casefold() == category.casefold()]
+    if books:
+        return books
+    else:
+        return {'error': 'Category not found'}
+
+
+@app.get("/books/{book_author}/")
+async def read_author_by_query(book_author: str, category: str):
+    books = [book for book in BOOKS
+             if book['author'].casefold() == book_author.casefold() and
+             book['category'].casefold() == category.casefold()]
+    if books:
+        return books
+    else:
+        return {'error': 'Author not found'}
